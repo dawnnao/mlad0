a4761955c7c4cd2563b03392e1d452f878f59021
dawnnao <dawnnao@gmail.com>
2016-12-29 19:02:13 +0800

    modify font size

--- monthStatsPerLabel.m
+++ monthStatsPerLabel.m
@@ -2,15 +2,18 @@ function f = monthStatsPerLabel(stats, labelNum, labelName, color)
 
 f = figure;
 h = bar3(stats);
-xlabel('Sensor');
-ylabel('Month');
-zlabel('Count (hours)');
-title(labelName);
+set(gca, 'fontsize', 14)
+xlabel('Sensor','fontsize',14);
+ylabel('Month','fontsize',14);
+zlabel('Count (hours)','fontsize',14);
+title(labelName,'fontsize',14);
 
 for n = 1 : size(stats, 2)
     set(h(n),'FaceColor', color{labelNum});
 end
 set(h, 'EdgeColor', [107 107 107]/255);
-set(gca, 'fontsize', 7)
+
+set(gcf,'Units','pixels','Position',[100, 100, 1000, 618]);  % control figure's position
+% set(gca,'Units','normalized', 'Position',[0 0 1 1]);  % control axis's position in figure
 
 end
--- monthStatsPerSensor.m
+++ monthStatsPerSensor.m
@@ -2,6 +2,7 @@ function f = monthStatsPerSensor(stats, sensor, labelName, color)
 
 f = figure;
 h = bar3(stats);
+set(gca, 'fontsize', 14)
 xlabel('Type');
 ylabel('Month');
 zlabel('Count (hours)');
@@ -13,6 +14,8 @@ for n = 1 : length(labelName)
     set(h(n),'FaceColor', color{n});
 end
 set(h, 'EdgeColor', [107 107 107]/255);
-set(gca, 'fontsize', 10);
+
+set(gcf,'Units','pixels','Position',[100, 100, 1000, 618]);  % control figure's position
+% set(gca,'Units','normalized', 'Position',[0 0 1 1]);  % control axis's position in figure
 
 end
