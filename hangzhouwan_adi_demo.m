6af721b5ba90f60b589c7b0f2d3f8dd0b0f0904f
dawnnao <dawnnao@gmail.com>
2017-01-03 11:54:30 +0800

    make demo mp4

---
+++ hangzhouwan_adi_demo.m
@@ -0,0 +1,15 @@
+clear;clc;close all;
+
+pathRoot = '/Volumes/midDisk/hangzhouwan-2016Q1-tidy/netmanager_b';
+sensorNum = [1];
+dateStart = '2016-01-01';
+dateEnd = '2016-01-01';
+sensorTrainRatio = 80/100;
+% sensorPSize = 10;
+step = [2 3 4];
+% labelName = {'1-normal','2-outlier','3-minor'};
+
+sensor = adidnn(pathRoot, sensorNum, dateStart, dateEnd, sensorTrainRatio,[], step,[]);
+
+
+
