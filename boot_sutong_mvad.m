79858df030405f9a3e6e23096b1913902dfe7c13
Zhiyi Tang <dawnnao@gmail.com>
2017-07-14 18:46:13 +0800

    update

--- boot_sutong_mvad.m
+++ boot_sutong_mvad.m
@@ -1,8 +1,8 @@
 clear;clc;close all;
 
 
-readRoot = 'H:/sutong-2012-tidy';
-saveRoot = 'F:/adi/case';
+readRoot = 'E:/dataArchive/Sutong';
+saveRoot = 'C:/Users/Owner/Documents/GitHub/adi/case';
 
 % saveRoot = 'F:/adi/case';
 
@@ -12,13 +12,13 @@ saveRoot = 'F:/adi/case';
 % readRoot = '/Volumes/ssd/sutong-2012-tidy';
 % saveRoot = '/Users/tangzhiyi/Documents/MATLAB/adi/case';
 
-% for n = 1 : 38, sensorNum{n} = n; end
-sensorNum = [32];
+for n = 1 : 38, sensorNum{n} = n; end
+% sensorNum = [32];
 dateStart = '2012-01-01';
-dateEnd = '2012-01-02';
+dateEnd = '2012-12-31';
 sensorTrainRatio = 1/100;
 sensorPSize = 10;
-step = [1];
+step = [5];
 % labelName = {'1-normal','2-missing','3-minor','4-outlier','5-square','6-trend up','7-trend down','8-trend random'};
 
 %%
