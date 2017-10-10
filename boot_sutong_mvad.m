c49fc024933292049c5e44b051f5f115fa3128ec
Zhiyi Tang <dawnnao@gmail.com>
2017-10-10 14:22:25 +0800

    do new case in paper 1

--- boot_sutong_mvad.m
+++ boot_sutong_mvad.m
@@ -18,12 +18,12 @@ saveRoot = 'E:/results/adi';
 % saveRoot = '/Users/tangzhiyi/Documents/MATLAB/adi/case';
 
 % for n = 1 : 38, sensorNum{n} = n; end
-sensorNum = [29:38];
+sensorNum = [1:38];
 dateStart = '2012-01-01';
 dateEnd = '2012-12-31';
 sensorTrainRatio = 1/100;
 sensorPSize = 10;
-step = [1];
+step = [4 5];
 % labelName = {'1-normal','2-missing','3-minor','4-outlier','5-square','6-trend up','7-trend down','8-trend random'};
 
 %%
